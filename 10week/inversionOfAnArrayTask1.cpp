20f9e7431bab3789f53327f3ac66136f0af158b8
unknown <martingotsev@gmail.com>
2022-01-02 23:34:57 +0200

    new commit

--- 10week/inversionOfAnArrayTask1.cpp
+++ 10week/inversionOfAnArrayTask1.cpp
@@ -3,39 +3,43 @@
 // ; Примерен вход и изход при аргументи {1,2,3,4,5} 5: 5 4 3 2 1
 #include <iostream>
 using namespace std;
-const int maxSize=10000;
+const int maxSize = 10000;
 void printArray(char *array)
 {
-    for(int i=0;array[i]!=0;i++)
+    for (int i = 0; array[i] != 0; i++)
     {
-        cout<<array[i];
+        cout << array[i];
     }
-    cout<<endl;
+    cout << endl;
 }
 int sizeOfArray(char *array)
 {
-    int size=0;
-    for(int i=0;array[i]!=0;i++)
+    int size = 0;
+    for (int i = 0; array[i] != 0; i++)
     {
         size++;
     }
     return size;
 }
 
-void revurstArray(char *array)
+void revurstArray(char *array, int size)
 {
-    int size = sizeOfArray(array);
-    if (array[0] == array[size])
+
+    if (size < 0)
         return;
-    char symbol = array[0];
-    
-
-
+    else
+    {
+        cout << "*";
+        char symbol = array[0];
+        array[0] = array[size];
+        array[size] = symbol;
+        return revurstArray(array + 1, size - 2);
+    }
 }
 int main()
 {
     char array[maxSize];
-    cin.getline(array,maxSize);
+    cin.getline(array, maxSize);
+    revurstArray(array, sizeOfArray(array) - 1);
     printArray(array);
-
 }
